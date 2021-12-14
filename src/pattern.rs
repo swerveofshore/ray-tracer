91366ac63c247cb1266cc541c2ab100995a7a3e0
M. Claver <52391021+swerveofshore@users.noreply.github.com>
2021-12-13 19:20:11 -0500

    Make Pattern an enumeration, not a trait object
    In terms of ownership, Material should be Copy (as nothing should really
    "own" Material, and Material shouldn't really "own" anything).

--- src/pattern.rs
+++ src/pattern.rs
@@ -1,79 +1,84 @@
 use crate::feq;
 use crate::tuple::Tuple4D;
+use crate::matrix::Matrix4D;
 use crate::color::Color;
 
-pub trait Pattern {
-    fn pattern_at(&self, p: Tuple4D) -> Color;
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum PatternType {
+    Stripe(Color, Color),
 }
 
-/// An alternating stripe pattern applied across the X axis.
-///
-/// Effectively, for a point `(x, y, z)`, if `floor(x) % 2 == 0`, the `primary'
-/// color is applied to that point; otherwise, the `secondary` color is used.
-#[derive(Copy, Clone, Debug, Default, PartialEq)]
-pub struct StripePattern {
-    primary: Color,
-    secondary: Color,
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Pattern {
+    ty: PatternType,
+    transform: Matrix4D,
 }
 
-impl StripePattern {
-    pub fn new(primary: Color, secondary: Color) -> StripePattern {
-        StripePattern { primary, secondary }
+impl Pattern {
+    pub fn stripe(transform: Matrix4D, primary: Color, secondary: Color)
+        -> Pattern {
+        Pattern { ty: PatternType::Stripe(primary, secondary), transform }
     }
 
-    pub fn stripe_at(&self, p: Tuple4D) -> Color {
-        if feq(p.x.floor().rem_euclid(2.0), 0.0) {
-            self.primary
-        } else {
-            self.secondary
+    pub fn pattern_at(&self, p: Tuple4D) -> Color {
+        match self.ty {
+            PatternType::Stripe(_, _) => self.stripe_at(p),
         }
     }
-}
 
-impl Pattern for StripePattern {
-    fn pattern_at(&self, p: Tuple4D) -> Color {
-        self.stripe_at(p)
+    fn stripe_at(&self, p: Tuple4D) -> Color {
+        // The pattern type here can't be anything *but* a Stripe.
+        let PatternType::Stripe(primary, secondary) = self.ty;
+
+        if feq(p.x.floor().rem_euclid(2.0), 0.0) {
+            primary
+        } else {
+            secondary
+        }
     }
 }
 
 #[test]
 fn stripe_pattern_is_constant_along_y() {
-    let pattern = StripePattern::new(Color::white(), Color::black());
+    let pattern = Pattern::stripe(Matrix4D::identity(),
+        Color::white(), Color::black());
 
-    assert_eq!(pattern.stripe_at(Tuple4D::point(0.0, 0.0, 0.0)),
+    assert_eq!(pattern.pattern_at(Tuple4D::point(0.0, 0.0, 0.0)),
         Color::white());
-    assert_eq!(pattern.stripe_at(Tuple4D::point(0.0, 1.0, 0.0)),
+    assert_eq!(pattern.pattern_at(Tuple4D::point(0.0, 1.0, 0.0)),
         Color::white());
-    assert_eq!(pattern.stripe_at(Tuple4D::point(0.0, 2.0, 0.0)),
+    assert_eq!(pattern.pattern_at(Tuple4D::point(0.0, 2.0, 0.0)),
         Color::white());
 }
 
 #[test]
 fn stripe_pattern_is_constant_along_z() {
-    let pattern = StripePattern::new(Color::white(), Color::black());
+    let pattern = Pattern::stripe(Matrix4D::identity(),
+        Color::white(), Color::black());
 
-    assert_eq!(pattern.stripe_at(Tuple4D::point(0.0, 0.0, 0.0)),
+    assert_eq!(pattern.pattern_at(Tuple4D::point(0.0, 0.0, 0.0)),
         Color::white());
-    assert_eq!(pattern.stripe_at(Tuple4D::point(0.0, 0.0, 1.0)),
+    assert_eq!(pattern.pattern_at(Tuple4D::point(0.0, 0.0, 1.0)),
         Color::white());
-    assert_eq!(pattern.stripe_at(Tuple4D::point(0.0, 0.0, 2.0)),
+    assert_eq!(pattern.pattern_at(Tuple4D::point(0.0, 0.0, 2.0)),
         Color::white());
 }
 
 #[test]
 fn stripe_pattern_alternates_along_x() {
-    let pattern = StripePattern::new(Color::white(), Color::black());
+    let pattern = Pattern::stripe(Matrix4D::identity(),
+        Color::white(), Color::black());
 
-    assert_eq!(pattern.stripe_at(Tuple4D::point( 0.0, 0.0, 0.0)),
+    assert_eq!(pattern.pattern_at(Tuple4D::point( 0.0, 0.0, 0.0)),
         Color::white());
-    assert_eq!(pattern.stripe_at(Tuple4D::point( 0.9, 0.0, 0.0)),
+    assert_eq!(pattern.pattern_at(Tuple4D::point( 0.9, 0.0, 0.0)),
         Color::white());
-    assert_eq!(pattern.stripe_at(Tuple4D::point( 1.0, 0.0, 0.0)),
+    assert_eq!(pattern.pattern_at(Tuple4D::point( 1.0, 0.0, 0.0)),
         Color::black());
-    assert_eq!(pattern.stripe_at(Tuple4D::point(-0.1, 0.0, 0.0)),
+    assert_eq!(pattern.pattern_at(Tuple4D::point(-0.1, 0.0, 0.0)),
         Color::black());
-    assert_eq!(pattern.stripe_at(Tuple4D::point(-1.0, 0.0, 0.0)),
+    assert_eq!(pattern.pattern_at(Tuple4D::point(-1.0, 0.0, 0.0)),
         Color::black());
-    assert_eq!(pattern.stripe_at(Tuple4D::point(-1.1, 0.0, 0.0)),
+    assert_eq!(pattern.pattern_at(Tuple4D::point(-1.1, 0.0, 0.0)),
         Color::white());
 }
