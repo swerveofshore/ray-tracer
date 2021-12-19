1264619c04113b7b04a38c54c991561e1c34c7ce
M. Claver <52391021+swerveofshore@users.noreply.github.com>
2021-12-19 13:24:31 -0700

    Remove debugging print statements from sphere intersection

--- src/shape.rs
+++ src/shape.rs
@@ -181,10 +181,8 @@ impl Shape {
         let a = ray.direction.dot(&ray.direction);
         let b = 2.0 * ray.direction.dot(&sphere_to_ray);
         let c = sphere_to_ray.dot(&sphere_to_ray) - 1.0;
-        println!("d dot d: {}", sphere_to_ray.dot(&sphere_to_ray));
 
         let discriminant = b.powi(2) - (4.0 * a * c);
-        println!("a: {}, c: {}", a, c);
 
         if discriminant < 0.0 {
             return Intersections::new()
