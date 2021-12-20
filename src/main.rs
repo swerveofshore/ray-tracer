eae6841d13977e4d08192be52903e41621831d93
M. Claver <52391021+swerveofshore@users.noreply.github.com>
2021-12-19 17:25:37 -0700

    Make a scene demonstrating blues

--- src/main.rs
+++ src/main.rs
@@ -31,62 +31,57 @@ fn main() {
         * Matrix4D::rotation_y(std::f64::consts::PI / 4.0);
 
     let mut floor = Shape::plane();
-    *floor.transform_mut() = Matrix4D::scaling(10.0, 0.01, 10.0);
-    *floor.material_mut() = Default::default();
-    floor.material_mut().color = Color::rgb(0.5, 0.5, 0.5);
-    floor.material_mut().specular = 0.0;
-    floor.material_mut().pattern = Some(floor_pattern);
-    floor.material_mut().reflective = 0.5;
+    floor.transform = Matrix4D::scaling(10.0, 0.01, 10.0);
+    floor.material = Default::default();
+    floor.material.color = Color::rgb(0.5, 0.5, 0.5);
+    floor.material.specular = 0.0;
+    floor.material.pattern = Some(floor_pattern);
+    floor.material.reflective = 0.5;
 
     let mut middle = Shape::sphere();
-    *middle.transform_mut() = Matrix4D::translation(-0.5, 1.0, -5.0);
-    *middle.material_mut() = Default::default();
-    middle.material_mut().color = Color::rgb(1.0, 0.4666, 0.2666);
-    middle.material_mut().diffuse = 0.7;
-    middle.material_mut().specular = 0.3;
-    middle.material_mut().transparency = 0.5;
-    middle.material_mut().reflective = 0.5;
-    // middle.material.refractive_index = 1.5;
+    middle.transform = Matrix4D::translation(-0.5, 1.0, -5.0);
+    middle.material = Default::default();
+    middle.material.color = Color::rgb(0.2, 0.2, 0.3);
+    middle.material.diffuse = 0.7;
+    middle.material.specular = 0.3;
+    middle.material.transparency = 0.5;
+    middle.material.reflective = 0.5;
+    middle.material.refractive_index = 2.0;
 
-    /*
-    middle.material.pattern = Some(Pattern::checker(
-        Color::blue(),
-        Color::green()
-    ));
-    */
-
-    let mut right = Shape::capped_cylinder(0.0, 3.0);
-    *right.transform_mut() = Matrix4D::translation(1.5, 2.5, -5.5)
+    let mut right = Shape::cube();
+    right.transform = Matrix4D::translation(1.5, 2.5, -5.5)
         * Matrix4D::scaling(0.25, 0.25, 0.25)
-        // * Matrix4D::rotation_x(std::f64::consts::PI / 1.0)
+        * Matrix4D::rotation_x(std::f64::consts::PI / 1.0)
         * Matrix4D::rotation_z(std::f64::consts::PI / 4.0)
         * Matrix4D::rotation_y(std::f64::consts::PI / 8.0);
-    *right.material_mut() = Default::default();
-    // right.material.color = Color::rgb(1.0, 0.6666, 0.2666);
-    right.material_mut().diffuse = 0.7;
-    right.material_mut().specular = 0.3;
-    right.material_mut().reflective = 0.3;
-    right.material_mut().transparency = 0.9;
-    right.material_mut().refractive_index = 1.5;
+    right.material = Default::default();
+    right.material.color = Color::rgb(0.1, 0.1, 0.5);
+    right.material.diffuse = 0.7;
+    right.material.specular = 0.3;
+    right.material.reflective = 0.3;
+    right.material.transparency = 0.9;
+    right.material.refractive_index = 1.5;
 
     let mut left = Shape::cube();
-    *left.transform_mut() = Matrix4D::translation(-1.5, 1.0, -0.75)
+    left.transform = Matrix4D::translation(-3.5, 1.0, -0.75)
         * Matrix4D::scaling(0.33, 0.33, 0.33)
         * Matrix4D::rotation_x(std::f64::consts::PI / 4.0)
         * Matrix4D::rotation_y(std::f64::consts::PI / 4.0)
         * Matrix4D::rotation_z(std::f64::consts::PI / 4.0);
-    *left.material_mut() = Default::default();
-    // left.material.color = Color::rgb(0.8666, 0.2, 0.2);
-    left.material_mut().diffuse = 0.7;
-    left.material_mut().specular = 0.3;
-    left.material_mut().transparency = 0.5;
-    left.material_mut().refractive_index = 1.1;
+    left.material = Default::default();
+    left.material.color = Color::rgb(0.3, 0.3, 0.7);
+    left.material.diffuse = 0.7;
+    left.material.specular = 0.3;
+    left.material.transparency = 0.5;
+    left.material.refractive_index = 1.1;
 
     let mut world = World::empty();
+
     world.light_source = PointLight::new(
         Color::rgb(1.0, 1.0, 1.0),
         Tuple4D::point(-10.0, 10.0, -10.0),
     );
+
     world.objects = vec![
         floor,
         middle,
