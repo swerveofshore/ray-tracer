d5a70a99ead426cb4d28f5f035c8ba7c50088b1d
M. Claver <52391021+swerveofshore@users.noreply.github.com>
2021-12-21 14:34:39 -0700

    Make hexagon scene from end of chapter 14

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,15 @@
-use std::rc::Rc;
-use std::cell::RefCell;
-
 use ray_tracer_challenge::tuple::Tuple4D;
 use ray_tracer_challenge::matrix::Matrix4D;
-use ray_tracer_challenge::shape::ShapeNode;
 use ray_tracer_challenge::color::Color;
-use ray_tracer_challenge::pattern::Pattern;
 use ray_tracer_challenge::light::PointLight;
 use ray_tracer_challenge::world::World;
 use ray_tracer_challenge::camera::Camera;
+use ray_tracer_challenge::extra::hexagon;
 
-const CANVAS_WIDTH: usize = 960 * 4;
-const CANVAS_HEIGHT: usize = 540 * 4; 
+const CANVAS_WIDTH: usize = 960;
+const CANVAS_HEIGHT: usize = 540; 
 
 fn main() {
-    let mut backdrop = ShapeNode::plane();
-    backdrop.transform = Matrix4D::translation(0.0, 0.0, 10.0)
-        * Matrix4D::rotation_x(std::f64::consts::PI / 2.0);
-    backdrop.material.pattern = Some(
-        Pattern::checker(
-            Color::black(),
-            Color::white()
-        )
-    );
-    backdrop.material.reflective = 0.8;
-
-    let mut parent = ShapeNode::cube();
-    parent.transform = Matrix4D::scaling(4.0, 4.0, 4.0)
-        * Matrix4D::rotation_x(std::f64::consts::PI / 8.0)
-        * Matrix4D::rotation_y(std::f64::consts::PI / 4.0)
-        * Matrix4D::rotation_z(std::f64::consts::PI / 8.0);
-    parent.material.color = Color::rgb(0.2, 0.2, 0.4);
-    parent.material.transparency = 0.5;
-    parent.material.reflective = 0.2;
-    parent.material.refractive_index = 2.0;
-
-    let mut child_01 = ShapeNode::cube();
-    child_01.transform = Matrix4D::scaling(2.0, 2.0, 2.0)
-        * Matrix4D::rotation_x(std::f64::consts::PI / 8.0)
-        * Matrix4D::rotation_y(std::f64::consts::PI / 4.0)
-        * Matrix4D::rotation_z(std::f64::consts::PI / 8.0)
-        * Matrix4D::translation(3.0, 0.0, 0.0);
-    child_01.material.color = Color::rgb(0.4, 0.4, 0.8);
-    child_01.material.transparency = 0.5;
-    child_01.material.reflective = 0.2;
-    child_01.material.refractive_index = 2.0;
-
     let mut world = World::empty();
 
     world.light_source = PointLight::new(
@@ -53,10 +17,12 @@ fn main() {
         Tuple4D::point(-10.0, 10.0, -10.0),
     );
 
+    let hex = hexagon();
+    hex.borrow_mut().transform =
+        Matrix4D::rotation_x(2.0 * std::f64::consts::PI / 3.0);
+
     world.objects = vec![
-        Rc::new(RefCell::new(backdrop)),
-        Rc::new(RefCell::new(parent)),
-        Rc::new(RefCell::new(child_01))
+        hex
     ];
 
     let mut camera = Camera::new(CANVAS_WIDTH, CANVAS_HEIGHT,
@@ -65,7 +31,8 @@ fn main() {
         Tuple4D::point(0.0, 1.5, -5.0),
         Tuple4D::point(0.0, 1.0,  0.0),
         Tuple4D::vector(0.0, 1.0, 0.0),
-    ) * Matrix4D::translation(0.0, 0.0, 48.0);
+    ) * Matrix4D::translation(0.0, 0.0, 10.0);
 
     let canvas = camera.render(&mut world);
-    canvas.save("out.ppm").unwrap(); }
+    canvas.save("out.ppm").unwrap();
+}
