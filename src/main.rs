a6e719d9cd0f2b4492a60314c1ff2107e2a8745e
M. Claver <52391021+swerveofshore@users.noreply.github.com>
2021-12-22 16:34:19 -0700

    Set sample scene to render teapot model

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
+use std::rc::Rc;
+
 use ray_tracer_challenge::tuple::Tuple4D;
 use ray_tracer_challenge::matrix::Matrix4D;
 use ray_tracer_challenge::color::Color;
 use ray_tracer_challenge::light::PointLight;
 use ray_tracer_challenge::world::World;
 use ray_tracer_challenge::camera::Camera;
-use ray_tracer_challenge::extra::hexagon;
+// use ray_tracer_challenge::extra::hexagon;
+use ray_tracer_challenge::obj::ObjParser;
 
-const CANVAS_WIDTH: usize = 960;
-const CANVAS_HEIGHT: usize = 540; 
+const CANVAS_WIDTH: usize = 240 * 16;
+const CANVAS_HEIGHT: usize = 135 * 16; 
+const OBJ_FILE: &'static str = "./models/teapot.obj";
+const OUT_FILE: &'static str = "./out.ppm";
 
 fn main() {
+    println!("Parsing OBJ file {}...", OBJ_FILE);
+    let mut obj_parser = ObjParser::new(OBJ_FILE);
+    obj_parser.parse();
+    println!("...done.");
+
     let mut world = World::empty();
 
     world.light_source = PointLight::new(
@@ -17,12 +27,8 @@ fn main() {
         Tuple4D::point(-10.0, 10.0, -10.0),
     );
 
-    let hex = hexagon();
-    hex.borrow_mut().transform =
-        Matrix4D::rotation_x(2.0 * std::f64::consts::PI / 3.0);
-
     world.objects = vec![
-        hex
+        Rc::clone(obj_parser.groups.get("").unwrap())
     ];
 
     let mut camera = Camera::new(CANVAS_WIDTH, CANVAS_HEIGHT,
@@ -31,8 +37,11 @@ fn main() {
         Tuple4D::point(0.0, 1.5, -5.0),
         Tuple4D::point(0.0, 1.0,  0.0),
         Tuple4D::vector(0.0, 1.0, 0.0),
-    ) * Matrix4D::translation(0.0, 0.0, 10.0);
+    ) * Matrix4D::translation(0.0, -2.0, 8.0);
 
+    println!("Rendering...");
     let canvas = camera.render(&mut world);
-    canvas.save("out.ppm").unwrap();
+    canvas.save(OUT_FILE).unwrap();
+    println!("...done.");
+    println!("PPM file saved to {}.", OUT_FILE);
 }
