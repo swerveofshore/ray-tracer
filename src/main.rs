5e6bed974fe9c552434fdff1a1c19118c7177109
M. Claver <52391021+swerveofshore@users.noreply.github.com>
2021-12-24 11:10:50 -0700

    Make default scene render all groups in OBJ file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::rc::Rc;
+use std::cell::RefCell;
 
 use ray_tracer_challenge::tuple::Tuple4D;
 use ray_tracer_challenge::matrix::Matrix4D;
 use ray_tracer_challenge::color::Color;
 use ray_tracer_challenge::light::PointLight;
+use ray_tracer_challenge::pattern::Pattern;
+use ray_tracer_challenge::shape::ShapeNode;
 use ray_tracer_challenge::world::World;
 use ray_tracer_challenge::camera::Camera;
-// use ray_tracer_challenge::extra::hexagon;
 use ray_tracer_challenge::obj::ObjParser;
 
-const CANVAS_WIDTH: usize = 125; // 1000; // 240 * 8;
-const CANVAS_HEIGHT: usize = 100; // 800; // 135 * 8; 
-const OBJ_FILE: &'static str = "./models/teddy.obj";
+const CANVAS_WIDTH: usize = 500; // * 8; // 240 * 8;
+const CANVAS_HEIGHT: usize = 400; // * 8; // 135 * 8; 
+const OBJ_FILE: &'static str = "./models/old-teapot.obj";
 const OUT_FILE: &'static str = "./out.ppm";
 
 fn main() {
@@ -20,19 +22,38 @@ fn main() {
     obj_parser.parse();
     println!("...done.");
 
-    let model = obj_parser.groups.get("").unwrap();
+    // let model = obj_parser.groups.get("").unwrap();
+    let mut models: Vec<_> =
+        obj_parser.groups.values().map(|g| Rc::clone(&g)).collect();
+
+    /*
     model.borrow_mut().transform
-        = Matrix4D::rotation_y(5.0 * std::f64::consts::PI / 4.0);
+        = Matrix4D::rotation_y(3.0 * std::f64::consts::PI / 4.0);
+
+    for child in model.borrow().children().unwrap() {
+        child.borrow_mut().material.transparency = 0.8;
+        child.borrow_mut().material.reflective = 0.1;
+        child.borrow_mut().material.refractive_index = 1.5;
+    }
+    */
+
+    let floor = Rc::new(RefCell::new(ShapeNode::plane()));
+    floor.borrow_mut().transform
+        = Matrix4D::translation(0.0, -4.0, 0.0);
+    floor.borrow_mut().material.pattern
+        = Some(Pattern::checker(Color::black(), Color::white()));
 
     let mut world = World::empty();
     world.light_source = PointLight::new(
-        Color::rgb(0.6, 0.8, 1.0),
+        Color::rgb(0.9, 0.9, 0.7),
         Tuple4D::point(-10.0, 10.0, -10.0),
     );
 
     world.objects = vec![
-        Rc::clone(&model)
+        // Rc::clone(&model),
+        Rc::clone(&floor),
     ];
+    world.objects.append(&mut models);
 
     let mut camera = Camera::new(CANVAS_WIDTH, CANVAS_HEIGHT,
         std::f64::consts::PI / 3.0, Matrix4D::identity());
@@ -40,7 +61,7 @@ fn main() {
         Tuple4D::point(0.0, 1.5, -5.0),
         Tuple4D::point(0.0, 1.0,  0.0),
         Tuple4D::vector(0.0, 1.0, 0.0),
-    ) * Matrix4D::translation(0.0, -16.0, 120.0);
+    ) * Matrix4D::translation(1.0, 0.0, 16.0);
 
     println!("Rendering...");
     let canvas = camera.render(&mut world);
