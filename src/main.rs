195a43b01c4dae57621bc7c4d94874f743f0c480
M. Claver <52391021+swerveofshore@users.noreply.github.com>
2022-01-05 14:34:26 -0700

    Add CLI parameters for specifying scenes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+use std::path::Path;
+
+use clap::{ app_from_crate, arg };
+use ray_tracer_challenge::scene::{ Scene, SceneJson };
+
 use ray_tracer_challenge::tuple::Tuple4D;
 use ray_tracer_challenge::matrix::Matrix4D;
 use ray_tracer_challenge::color::Color;
@@ -13,57 +19,79 @@ use ray_tracer_challenge::consts::{ CANVAS_WIDTH, CANVAS_HEIGHT };
 const OBJ_FILE: &'static str = "./models/old-teapot.obj";
 
 fn main() {
-    println!("Parsing OBJ file {}...\n", OBJ_FILE);
-    let mut obj_parser = ObjParser::new(OBJ_FILE);
-    obj_parser.parse();
-    println!("...done.");
+    let matches = app_from_crate!()
+        .arg(
+            arg!(
+                -s --scene <FILE> "Sets a scene to be rendered."
+            )
+            .required(false)
+            .allow_invalid_utf8(true),
+        )
+        .get_matches();
 
-    // For default group in the OBJ file:
-    // let model = obj_parser.groups.get("").unwrap();
+    if let Some(raw_scene) = matches.value_of_os("scene") {
+        let scene_path = Path::new(raw_scene);
+        let scene_str = fs::read_to_string(scene_path).expect(
+            &format!("Failed to read scene JSON file {:?}.", scene_path)
+        );
 
-    // For multiple groups in the OBJ file:
-    let mut models: Vec<_> = obj_parser.groups.values().cloned().collect();
+        let scene_json: SceneJson
+            = serde_json::from_str(&scene_str).expect(
+                &format!("Failed to parse scene JSON file {:?}", scene_path)
+            );
 
-    let mut floor = Shape::plane();
-    floor.set_transform(Matrix4D::translation(0.0, -4.0, 0.0));
-    floor.material.pattern
-        = Some(Pattern::checker(Color::black(), Color::white()));
-    floor.material.reflective = 0.8;
+        let scene: Scene = scene_json.into();
+        parallel_render(scene.world, scene.camera);
+    } else {
+        println!("Parsing OBJ file {}...", OBJ_FILE);
+        let mut obj_parser = ObjParser::new(OBJ_FILE);
+        obj_parser.parse();
+        println!("...done.\n");
 
-    let mut cube = Shape::cube();
-    cube.set_transform(Matrix4D::scaling(0.75, 0.75, 0.75)
-        * Matrix4D::rotation_y(std::f64::consts::PI / 8.0)
-        * Matrix4D::rotation_z(std::f64::consts::PI / 8.0));
-    cube.material.color = Color::rgb(1.0, 1.0, 0.0);
+        // For multiple groups in the OBJ file:
+        let mut models: Vec<_> = obj_parser.groups.values().cloned().collect();
 
-    let mut sphere = Shape::sphere();
-    sphere.set_transform(Matrix4D::rotation_y(std::f64::consts::PI / 8.0)
-        * Matrix4D::translation(0.0, 0.0, 0.0));
-    sphere.material.color = Color::red();
+        let mut floor = Shape::plane();
+        floor.set_transform(Matrix4D::translation(0.0, -4.0, 0.0));
+        floor.material.pattern
+            = Some(Pattern::checker(Color::black(), Color::white()));
+        floor.material.reflective = 0.8;
 
-    let mut difference = Shape::csg_difference(cube, sphere);
-    difference.set_transform(Matrix4D::translation(-5.0, 0.0, 0.0));
+        let mut cube = Shape::cube();
+        cube.set_transform(Matrix4D::scaling(1.0, 1.0, 1.0)
+            * Matrix4D::rotation_y(std::f64::consts::PI / 8.0)
+            * Matrix4D::rotation_z(std::f64::consts::PI / 8.0));
+        cube.material.color = Color::rgb(1.0, 1.0, 0.0);
 
-    let mut world = World::empty();
-    world.light_source = PointLight::new(
-        Color::rgb(0.85, 0.8, 0.65),
-        Tuple4D::point(-10.0, 10.0, -10.0),
-    );
+        let mut sphere = Shape::sphere();
+        sphere.set_transform(Matrix4D::rotation_y(std::f64::consts::PI / 8.0)
+            * Matrix4D::translation(0.75, 0.0, 0.0));
+        sphere.material.color = Color::red();
 
-    world.objects = vec![
-        floor,
-        difference,
-    ];
-    world.objects.append(&mut models);
+        let mut difference = Shape::csg_difference(cube, sphere);
+        difference.set_transform(Matrix4D::translation(-5.0, 0.0, 0.0));
 
-    let mut camera = Camera::new(CANVAS_WIDTH, CANVAS_HEIGHT,
-        std::f64::consts::PI / 3.0, Matrix4D::identity());
-    camera.transform = Matrix4D::view_transform(
-        Tuple4D::point(0.0, 1.5, -5.0),
-        Tuple4D::point(0.0, 1.0,  0.0),
-        Tuple4D::vector(0.0, 1.0, 0.0),
-    ) * Matrix4D::translation(2.0, 0.0, 12.0);
+        let mut world = World::empty();
+        world.light_source = PointLight::new(
+            Color::rgb(0.85, 0.8, 0.65),
+            Tuple4D::point(-10.0, 10.0, -10.0),
+        );
 
-    // Render the world.
-    parallel_render(world, camera);
+        world.objects = vec![
+            floor,
+            difference,
+        ];
+        world.objects.append(&mut models);
+
+        let mut camera = Camera::new(CANVAS_WIDTH, CANVAS_HEIGHT,
+            std::f64::consts::PI / 3.0, Matrix4D::identity());
+        camera.transform = Matrix4D::view_transform(
+            Tuple4D::point(0.0, 1.5, -5.0),
+            Tuple4D::point(0.0, 1.0,  0.0),
+            Tuple4D::vector(0.0, 1.0, 0.0),
+        ) * Matrix4D::translation(2.0, 0.0, 12.0);
+
+        // Render the world.
+        parallel_render(world, camera);
+    }
 }
